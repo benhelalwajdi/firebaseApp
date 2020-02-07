1dcee17aefda67f939da77b4aa8e523a6bf7404a
wajdibenhelal <wajdibenhelal52@live.fr>
2020-02-07 14:46:00 +0100

    Add firebase API method and create account with email and password

--- app/src/main/java/com/wbh/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/wbh/firebaseapp/MainActivity.kt
@@ -6,6 +6,9 @@ import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private var login_btn: Button?= null
     private var email : EditText?= null
     private var password: EditText?= null
+    private var firebaseAuth: FirebaseAuth?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         login_btn = findViewById(R.id.login);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        firebaseAuth = FirebaseAuth.getInstance()
+
 
         register_btn?.setOnClickListener {
             registerNewUser()
@@ -40,10 +46,19 @@ class MainActivity : AppCompatActivity() {
         if (TextUtils.isEmpty(email_text)){
             Toast.makeText(applicationContext,"this field is empty", Toast.LENGTH_LONG).show()
         }
-
-
-        if (TextUtils.isEmpty(password_text)){
+        else if (TextUtils.isEmpty(password_text)){
             Toast.makeText(applicationContext,"this field is empty", Toast.LENGTH_LONG).show()
+        }else {
+            firebaseAuth?.createUserWithEmailAndPassword(email_text,password_text)?.addOnCompleteListener(object: OnCompleteListener<AuthResult>{
+                override fun onComplete(p0: Task<AuthResult>) {
+                    if (p0.isSuccessful){
+                        Toast.makeText(applicationContext,"Account Created", Toast.LENGTH_LONG).show()
+                    }else{
+                        val error = p0.exception?.message
+                        Toast.makeText(applicationContext,error.toString(),Toast.LENGTH_LONG).show()
+                    }
+                }
+            });
         }
     }
 }
