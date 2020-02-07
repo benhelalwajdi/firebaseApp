bbd4c3c4bfa79427cc1a7197ec4b3e57c3712bc6
wajdibenhelal <wajdibenhelal52@live.fr>
2020-02-07 16:16:20 +0100

    create class UserService for more structure in the code then all the function like  sharedPreferences set in this class

---
+++ app/src/main/java/com/wbh/firebaseapp/Services/UserService.kt
@@ -0,0 +1,14 @@
+package com.wbh.firebaseapp.Services
+
+import android.content.Context
+
+class UserService {
+
+    private var ctx: Context? = null
+
+    public fun UserService(cn: Context){
+        this.ctx = cn
+    }
+
+    /*TODO sharedPreferences*/
+}
