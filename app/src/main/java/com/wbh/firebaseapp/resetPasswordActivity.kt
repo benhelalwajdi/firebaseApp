85746d9810961801144399f8de689cb6015207a9
wajdibenhelal <wajdibenhelal52@live.fr>
2020-02-07 16:04:59 +0100

    create ResetActivity

---
+++ app/src/main/java/com/wbh/firebaseapp/resetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.wbh.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
