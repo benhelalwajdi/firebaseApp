20dae1d35af1e3530bbe7a18d0f6fc0e469068b6
wajdibenhelal <wajdibenhelal52@live.fr>
2020-02-10 18:22:30 +0100

    model Movie for get the data from json file

---
+++ app/src/main/java/com/wbh/firebaseapp/Model/Movie.kt
@@ -0,0 +1,13 @@
+package com.wbh.firebaseapp.Model
+
+import com.google.gson.JsonArray
+
+class Movie(
+    var original_name: String?, var genre_ids: JsonArray,
+    var name: String?, var origin_country: JsonArray,
+    var vote_count: String?, var first_air_date: String?,
+    var backdrop_path: String?, var original_language: String,
+    var id: String?, var vote_average: String,
+    var overview: String?, var poster_path: String?
+) {
+}
